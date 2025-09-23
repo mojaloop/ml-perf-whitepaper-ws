e98601604aedf44ff833ee6eb4e3976eee0a8e08
kiswend <nathan@delma.me>
2025-09-23 14:42:38 +0000

    split db nodes

--- phases/02-infrastructure/terraform/main.tf
+++ phases/02-infrastructure/terraform/main.tf
@@ -25,6 +25,8 @@ locals {
   network_config  = local.config_file.network
   security_config = local.config_file.security
   vms_config      = local.config_file.vms
+  k8s_config      = local.config_file.k8s
+  lb_config       = try(local.config_file.load_balancers, {})
 
   # Common tags for all resources
   common_tags = {
@@ -37,8 +39,16 @@ locals {
   # Parse VM configurations
   bastion_config = local.vms_config.bastion
   switch_instances = local.vms_config.switch.instances
+  switch_defaults = local.vms_config.switch.defaults
   dfsp_instances = local.vms_config.dfsps.instances
   dfsp_defaults = local.vms_config.dfsps.defaults
+
+  # Performance settings
+  placement_group_enabled = try(local.aws_config.placement_group.enabled, false)
+  detailed_monitoring = try(local.aws_config.cloudwatch.detailed_monitoring, false)
+
+  # Load balancer settings
+  nlb_config = try(local.lb_config.switch_nlb, null)
 }
 
 # Configure AWS Provider
@@ -54,4 +64,20 @@ provider "aws" {
 # Data source for availability zones
 data "aws_availability_zones" "available" {
   state = "available"
+}
+
+# Placement Group for all instances (performance optimization)
+resource "aws_placement_group" "cluster" {
+  count = local.placement_group_enabled ? 1 : 0
+
+  name     = "${local.project_config.name}-cluster-pg"
+  strategy = "cluster"  # All instances in same physical rack for lowest latency
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${local.project_config.name}-cluster-pg"
+      Purpose = "High-performance cluster placement"
+    }
+  )
 }
